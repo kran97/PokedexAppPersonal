9009d78c947783fca0ea7a6b6c13cff876dbc828
Karan Gupta <kgupta.1697@gmail.com>
2023-07-22 00:39:39 +0530

    Adding optional chaining and fixes

--- src/components/PokemonTitle.js
+++ src/components/PokemonTitle.js
@@ -2,8 +2,8 @@ import classes from './PokemonTitle.module.css';
 function PokemonTitle(props) {
     return (
         <section className={classes.titleHeader}>
-            <h2>{props.info.name}</h2>
-            <h2></h2>
+            <h2>{props?.info?.name}</h2>
+            <h2>{props?.description}</h2>
         </section>
     );
 }
